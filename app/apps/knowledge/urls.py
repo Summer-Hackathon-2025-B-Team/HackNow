ad59c197f9cfec9727ae2ba7bca7a4750c831f54
hanafudabonchan <lanz84111@gmail.com>
2025-08-17 17:24:23 +0900

    ナレッジ管理

--- app/apps/knowledge/urls.py
+++ app/apps/knowledge/urls.py
@@ -1,2 +1,17 @@
+# Djangoのurlモジュール(config/urls.py)からpath関数を読み込む
+from django.urls import path
+# 同じ階層のディレクトリにあるviewsモジュールを読み込む . は現在のディレクトリ
+from . import views
+
+app_name = 'knowledge'
+
+
 # DjangoがどのURLでどの処理(ビュー)を呼び出すかを判断するためのリスト
-urlpatterns = []
+urlpatterns = [
+    path('', views.KnowledgeListView.as_view(), name='index'),
+    path('create/', views.KnowledgeCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.KnowledgeDetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.KnowledgeUpdateView.as_view(), name='edit'),
+    path('<int:pk>/delete/', views.KnowledgeDeleteView.as_view(), name='delete'),
+
+]
